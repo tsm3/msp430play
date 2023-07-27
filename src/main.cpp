8d885f14b626b4a474712b646b577ae802a7e412
tsm3 <tristan.slayde.mansfield@gmail.com>
2023-07-27 16:35:47 -0500

    Finally made an LED on Port 1.6 blink at around 2Hz

--- src/main.cpp
+++ src/main.cpp
@@ -14,32 +14,52 @@
 // }
 
 void setup() {
+
+  WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+
   /* RED_LED is P1.0 GREEN_LED is P1.6 */
-  // pinMode(GREEN_LED, OUTPUT);
-  // pinMode(RED_LED, OUTPUT);
+  
 
   /* Set RED_LED and GREEN_LED to outputs, set RED on */
-  P1DIR |= BIT0 + BIT6;
-  P1OUT &= ~(BIT0 + BIT6);
-  P1OUT |= BIT0;
+  // P1DIR |= BIT0 + BIT6;
+  // P1OUT &= ~(BIT0 + BIT6);
+  // P1OUT |= BIT0;
 
   /* Configure DCOCLK to run at 1MHz */
+  DCOCTL = 0;
+  BCSCTL1 = CALBC1_1MHZ;
   DCOCTL = CALDCO_1MHZ;
 
   /* Configure SMCLK to div DCOCLK by 8 */
   BCSCTL2 &= ~SELS; // set SMCLK to DCOCLK
   BCSCTL2 |= DIVS_3; // div by 8
+  // BCSCTL2 |= DIVS_2; // div by 4
+  /* Now, SMCLK runs at 1e6/8= 125kHz */
 
   /* Configure TIMER_A to run on SMCLK and in Continuous Mode */
   TA0CTL = TASSEL_2 + MC_2;
+  // TA0CTL = TASSEL_1 + MC_2;
 
-  /* Configure */
+  /* Configure Timer_A Capture/Compare Control 0 Register TACCTL2 as set/reset*/
+  /* Want this to generate a pulse on some pin at 2Hz ? */
+  TA0CCTL1 = OUTMOD_3;
 
+  /* Configure the pulse by setting PWM period in TAOCCR1 */
+  TA0CCR0 = 62500-1; // This is about 2Hz (Reset)
+  TA0CCR1 = 62500-6250-1;  // Want about 10% duty cycle? (Set)
+
+  /* Set some output pins to the set/reset function */
+  // P1DIR |= BIT5;
+  // P1SEL |= BIT5;
+  // P1SEL2 &= ~BIT5;
+  
+  P1DIR |= BIT6;
+  P1SEL |= BIT6;
+  P1SEL2 &= ~BIT6;
 
 }
 
 void loop() {
-  __delay_cycles(8000000);
-  P1OUT ^= BIT0 + BIT6;
-  
+  // __delay_cycles(8000000);
+  // P1OUT ^= BIT0 + BIT6;
 }
